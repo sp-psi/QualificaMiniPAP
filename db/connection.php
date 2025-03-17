022a6713fde84d4c6f8ce929db4eb5eca15b3b11
sp-psi <sofiapinto@estel.edu.pt>
2025-03-17 22:31:34 +0000

    Add files via upload

---
+++ db/connection.php
@@ -0,0 +1,15 @@
+<?php
+function pdo_connect_mysql() {
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = '';
+    $DATABASE_NAME = 'qualifica_db';
+    try {
+    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+    } catch (PDOException $exception) {
+    	// Exibe mensagem caso ocorra erro
+        error_log($exception->getMessage());
+    	exit('Não foi possivel conectar base de dados!');
+    }
+}
+?>
